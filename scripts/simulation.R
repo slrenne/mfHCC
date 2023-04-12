2f021e5619de768aebe7f3c924f951fa4d8920f6
RENNE Salvatore HUNIMED <srenne@hunimed.eu>
2023-04-12 15:24:27 +0200

    db simulation from the dag

---
+++ scripts/simulation.R
@@ -0,0 +1,105 @@
+# set seed
+set.seed(230412) 
+
+# set the patient number that correspond to the patient's ID
+N <- 1e4
+k <- 1:N
+
+# let's start from the number of new clones j for each patient
+# let's also assume that j is a function of the background liver disease (B)
+# and the time the patient has it (Tj); the latter is itself a function of 
+# the patient's age.
+
+# so starting from the age (A) it can be simulated as a normal distribution
+
+A <- rnorm( n = N, mean = 0, sd = 1 ) 
+
+# this values can also be used as priors if the age is standardized (see ?scale )
+# next we can simulate the Tj that in the model will be a latent variable
+
+Tj <- rnorm( n = N , mean = A, sd = 1)
+
+# background liver disease 
+# looking at the DB I had some difficulty to find something clear
+# therefore for the time being I used a normal distribution
+
+## TO DISCUSS ##
+
+B <- rnorm( n = N, mean = 0, sd = 1 )
+
+# now we can simulate the number of new clones for each patient
+# the number of nodules can be simulated with a zero truncated Poisson 
+# distribution and as a link a exponential function 
+
+# the coefficients value can be changed but are chosen to provide
+# here we select some value trough prior predictive simulation
+# see the graphical check
+alpha_j <-  rnorm( n = N, mean = 0.2, sd = 0.2 )
+beta_j <- rnorm( n = N, mean = 0.3, sd = 0.1 )
+gamma_j <- rnorm( n = N, mean = 0, sd = 0.1 )
+lm <- alpha_j + beta_j * B + gamma_j * Tj
+lambda <- exp( lm )
+j <- actuar::rztpois(n = N, lambda = lambda)
+
+# a quick graphical check 
+d_j <- density(j)
+plot(d_j)
+
+# so up to now we simulated the number of new clones for each patient
+# however the total number of nodules depends also from the intra-hepatic 
+# metastases of each clone j of each patient 
+
+# to estimate the metastatic potential of each clone j of each patient k 
+# let's prepare a matrix both for the EMT and the VETC
+
+col.max <- max(j) # the columns needs to be the max of j
+
+# we will store the EMT of each clone in the matrix Ekj
+# let's suppose that the EMT will be present in a 20% of clones
+
+Ekj <- matrix( data = NA, nrow = N, ncol = col.max)
+for ( i in k ) {
+  for( ii in 1 : j[ i ] ){
+    Ekj[ i, ii ] <-  rbinom( n = 1, size = 1 , prob = 0.2 ) 
+  }
+}
+
+# now let's do the same for VETC
+# let's suppose that the VETC will be present in a 30% of clones
+
+Vkj <- matrix( data = NA, nrow = N, ncol = col.max)
+for ( i in k ) {
+  for( ii in 1 : j[ i ] ){
+    Vkj[ i, ii ] <-  rbinom( n = 1, size = 1 , prob = 0.3 ) 
+  }
+}
+
+
+# then there will be a time for each clone to metastasize 
+# and it will be a function of the patient's age
+
+Ti_kj <- matrix( data = NA, nrow = N, ncol = col.max)
+for ( i in k ) {
+  for( ii in 1 : j[ i ] ){
+    Ti_kj[ i, ii ] <-  rnorm( n = 1 , mean = A[i], sd = 1) 
+  }
+}
+
+# now let simulate the number of intrahepatic mets for each clone
+# we will use a Poisson distribution 
+# keeping the simulation 
+alpha_ji <- 0.2
+beta_ji <- 0.1
+gamma_ji <- 0.1
+
+
+i_kj <- matrix( data = NA, nrow = N, ncol = col.max)
+for ( i in k ) {
+  for( ii in 1 : j[ i ] ){
+    lm <- alpha_ji +  
+      beta_ji * (Ekj [i,ii] + Vkj [i, ii]) +
+      gamma_j * Ti_kj[ i, ii ]
+    lambda <- exp( lm )
+    i_kj[ i, ii ] <-  rpois( n = 1, lambda = lambda) 
+  }
+}
